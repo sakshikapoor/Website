a132deb69a06f58cc60e91c56dcfc8715544346e
Sakshi <sakshi.kapoor@box8.in>
2019-10-11 10:47:05 +0530

    added addcontent page

--- ResumeMaker/src/app/add-content/add-content.component.ts
+++ ResumeMaker/src/app/add-content/add-content.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-content',
   templateUrl: './add-content.component.html',
-  styleUrls: ['./add-content.component.css']
+  styleUrls: ['./add-content.component.scss']
 })
 export class AddContentComponent implements OnInit {
 
