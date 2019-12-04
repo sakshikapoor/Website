3963d2c4e4f47db197bf022f47e5fd1426c0cf4f
Sakshi <sakshi.kapoor@box8.in>
2019-12-04 11:27:29 +0530

    made layout page

--- ResumeMaker/src/app/layout/layout.component.ts
+++ ResumeMaker/src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cookieService: CookieService) { }
 
   ngOnInit() {
+  }
+
+  saveTemplate() {
+    this.cookieService.set('data', 'hh');
   }
 
 }
