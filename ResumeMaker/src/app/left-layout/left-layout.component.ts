5fe907dcbb686298f2c0645c66d9a5a6cc22e01f
Sakshi <sakshi.kapoor@box8.in>
2019-12-20 00:04:17 +0530

    made blank pages for left and right layouts

--- ResumeMaker/src/app/left-layout/left-layout.component.ts
+++ ResumeMaker/src/app/left-layout/left-layout.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-left-layout',
   templateUrl: './left-layout.component.html',
-  styleUrls: ['./left-layout.component.css']
+  styleUrls: ['./left-layout.component.scss']
 })
 export class LeftLayoutComponent implements OnInit {
 
